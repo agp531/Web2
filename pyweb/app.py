94f0e54be69b63fd5ffd77b5b4d3aca33c597e62
agp <allan.g531@hotmai.com>
2022-10-19 21:57:54 -0300

    back

--- pyweb/app.py
+++ pyweb/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 from datetime import datetime
 
 #Flask é um framework web
@@ -28,7 +29,33 @@ def cotacao():
     return render_template(
         "cotacao.html", valor = valor
         )
+    
+@app.route("/conversao", methods=["POST","GET"])
+def conversao():
+    # (GET) é o caso quando usuario digita a url on browser
+    if request.method == "GET":
+       return render_template("conversao.html")
+    else:
+        #(POST) e quando envia o formulario (clica calcular)
+        preco = float(request.form.get("preco"))
+        # TODO: pegar o preço da cotacao em:
+        # https://docs.awesomeapi.com.br/api-de-moedas
+        valor_reais = preco * 5.24
 
+        return render_template(
+            "conversao.html", valor_reais = valor_reais)
+
+@app.route("/imc", methods=["POST","GET"])
+def calculaImc():
+    if request.method == "GET":
+         return render_template("imc.html")
+    else:
+        peso = float(request.form.get("peso"))
+        altura = float(request.form.get("altura"))
+        total = peso / (altura * altura)
+        return render_template(
+            "imc.html", total = total
+        )
 
 
 if __name__ == "__main__":
---
+++ pyweb/aula3.md
@@ -0,0 +1,21 @@
+# Modelo de programação Web:
+## Em duas partes
+- Lado do cliente (navegador,aplicativo (mobile))
+- Lado do servidor (Python > (flask), PHP > (apache),...)
+
+## Modelo cliente-servidor
+- Trabalha com Requisicoes e respostas
+    - request / response
+
+- Requisição (GET) (quando pedimos algo para o servidor)
+- Resposta do servidor (geralmente HTML)
+
+## Implementação:
+
+### Fazer um formulário do html (cliente)
+- informa o preço do produto em dolar
+- exibe o valor em reais
+
+Recebe o preço do produto (servidor)
+- calcula o resultado preço ($) * 5.24
+- retorna uma "respota"
